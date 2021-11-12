732f7bd060a277f1a5ab304f838961ea1bf26395
Nitin George Cherian <realnitinworks@gmail.com>
2021-11-12 14:05:47 +0530

    Add base project structure

---
+++ project/app/main.py
@@ -0,0 +1,18 @@
+# project/app/main.py
+
+
+from fastapi.params import Depends
+from app.config import Settings, get_settings
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+
+@app.get("/ping")
+async def pong(settings: Settings = Depends(get_settings)):
+    return {
+        "ping": "pong",
+        "environment": settings.environment,
+        "testing": settings.testing,
+    }
