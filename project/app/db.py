ed1545c0c80d4c601e41b0b42f31930f8d3152ca
Nitin George Cherian <realnitinworks@gmail.com>
2021-11-13 14:08:12 +0530

    Refactor: Use APIRouter, add db init function and pydantic model

--- project/app/db.py
+++ project/app/db.py
@@ -2,8 +2,17 @@
 
 
 import os
+import logging
+
+from fastapi import FastAPI
+from tortoise.contrib.fastapi import register_tortoise
+from tortoise import Tortoise, run_async
 
 
+log = logging.getLogger("uvicorn")
+
+
+# For aerich to do migrations
 TORTOISE_ORM = {
     "connections": {"default": os.environ.get("DATABASE_URL")},
     "apps": {
@@ -13,3 +22,35 @@ TORTOISE_ORM = {
         },
     },
 }
+
+
+# generate_schema set to False - Tortoise does not do migrations
+# allows the flexibility to allow aerich or Tortoise to do migration at later point
+def init_db(app: FastAPI) -> None:
+    register_tortoise(
+        app,
+        db_url=os.environ.get("DATABASE_URL"),
+        modules={"models": ["app.models.tortoise"]},
+        generate_schemas=False,
+        add_exception_handlers=True,
+    )
+
+
+# Sometimes we need to do only the final migration
+# Tell tortoise to do apply the final migration
+# python app/db.py
+async def generate_schema() -> None:
+    log.info("Intializing Tortoise...")
+
+    await Tortoise.init(
+        db_url=os.environ.get("DATABASE_URL"),
+        modules={"models": ["models.tortoise"],}
+    )
+
+    log.info("Generating database schema via Tortoise...")
+    await Tortoise.generate_schemas()
+    await Tortoise.close_connections()
+
+
+if __name__ == "__main__":
+    run_async(generate_schema())
--- project/tests/conftest.py
+++ project/tests/conftest.py
@@ -8,7 +8,7 @@ from starlette import testclient
 import pytest
 from starlette.testclient import TestClient
 
-from app import main
+from app.main import create_application
 from app.config import get_settings, Settings
 
 
@@ -19,8 +19,9 @@ def get_settings_override():
 @pytest.fixture(scope="module")
 def test_app():
     # set up
-    main.app.dependency_overrides[get_settings] = get_settings_override
-    with TestClient(main.app) as test_client:
+    app = create_application()
+    app.dependency_overrides[get_settings] = get_settings_override
+    with TestClient(app) as test_client:
 
         # testing
         yield test_client
