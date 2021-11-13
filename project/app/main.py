9f7974e790d078f798b48d1135f5098d519d1a92
Nitin George Cherian <realnitinworks@gmail.com>
2021-11-13 12:33:46 +0530

    Get pytest up and running

--- project/app/main.py
+++ project/app/main.py
@@ -23,7 +23,7 @@ register_tortoise(
 @app.get("/ping")
 async def pong(settings: Settings = Depends(get_settings)):
     return {
-        "ping": "pong",
+        "ping": "pong!",
         "environment": settings.environment,
         "testing": settings.testing,
     }
